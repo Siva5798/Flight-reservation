0f8a4ab4a9872a52b0bb4173d464d1e70d95d66c
Siva5798 <sivathanu94@gmail.com>
2024-05-21 16:12:47 +0530

    code is update

--- Code.java
+++ Code.java
@@ -1,6 +1,6 @@
 public class Code{
 
     public static void main(String args[]){
-        System.out.println("Siva Thanu M S");
+        System.out.println("Siva Thanu M S the upcoming millionare ");
     }
 }
